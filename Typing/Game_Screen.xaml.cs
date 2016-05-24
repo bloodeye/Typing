05aff1d5aa2e2eba87a0014a132a8ad1de737b61
bloodeye <bob.bloodeye@gmail.com>
2016-05-24 23:11:35 +0700

    Handmade

--- Typing/Game_Screen.xaml.cs
+++ Typing/Game_Screen.xaml.cs
@@ -22,58 +22,44 @@ namespace Typing
     public partial class Game_Screen : Window
     {
         //khai bao bien
+        bool flag = true;
         public const int time_limit = 20;
-        public delegate void UpdateTextBackCall( TextBlock obj,string text);
-        public delegate void UpdatePostionBackCall(TextBlock obj,int W, int H);
-        public delegate void GetTextBlockBackCall(TextBlock obj);
-        public delegate void ResizeTextBlockBackCall(TextBlock obj);
+        public delegate void UpdateTextBackCall(TextBlock obj, string s);
+        public delegate void UpdateBackCall();
+        List<RichTextBox> words = new List<RichTextBox>();
+        List<string> lib = new List<string>();
+        int[] pos = new int[15];
+
         //ket thuc khai bao bien
 
         public Game_Screen()
         {
+            char iword;
+
             InitializeComponent();
+            for (int i = 97; i < 122; i++)
+            {
+                iword = (char)i;
+                lib.Add(iword.ToString());
+            }
+
+            makePlaceWords();
             Thread clock = new Thread(clock_event);
-            
-            Thread word = new Thread(game_run);
-            word.SetApartmentState(ApartmentState.STA);
-            word.Start();
+            Thread game = new Thread(game_run);
             clock.Start();
-            TextBlock a = new TextBlock();
-            a = (TextBlock)this.Resources["word"];
-            a.Text = "abc";
-            UpdatePosition(a, 50, 50);
-            a.ActualHeight.Equals(56);
-            a.ActualWidth.Equals(56);
-            //TextBlock a = (TextBlock) this.Resources["word"];
-            //a.Text = "abc";
+            game.Start();
+            
         }
 
         public void game_run()
         {
-           
-            int iword = (new Random().Next(97, 122));
-            char a = (char)iword;
-            TextBlock obj = new TextBlock();
-            //word_block.Dispatcher.Invoke(new UpdateTextBackCall(this.UpdateWord), new object[] { a.ToString() });
-            //iword = (new Random().Next(0, 500));
-            //a = (char)iword;
-
-            //viet chu cho word_block
-            obj.Dispatcher.Invoke(new GetTextBlockBackCall(this.GetTextBlock), new object[] { obj });
-
-            obj.Dispatcher.Invoke(new UpdateTextBackCall(this.UpdateWord), new object[] { obj,a.ToString()});
-
-
+            int iword;
             iword = (new Random().Next(0, 550));
-            for (int i = 0; i < 35; i++)
+            while (flag)
             {
-                //word_block.Dispatcher.Invoke(new UpdatePostionBackCall(this.UpdatePosition), new object[] {iword , i*20});
-                obj.Dispatcher.Invoke(new UpdatePostionBackCall(this.UpdatePosition), new object[] { obj,iword, i * 20 });
-
+                Dispatcher.Invoke(new UpdateBackCall(this.UpdatePosition));
                 Thread.Sleep(100);
-
             }
-
         }
 
         public void clock_event()
@@ -81,38 +67,81 @@ namespace Typing
             for (int i = 0; i < time_limit; i++)
             {
                 game_clock.Dispatcher.Invoke(new UpdateTextBackCall(this.UpdateClock), new object[] { game_clock,(i+1).ToString() });
+                Dispatcher.Invoke(new UpdateBackCall(this.CheckandSet));
                 Thread.Sleep(1000);
             }
             MessageBox.Show("End Game!");
-        }
-
-        public void UpdateWord(TextBlock obj,string word)
-        {
-            obj.Text = word;
+            
         }
         public void UpdateClock(TextBlock obj,string clock)
         {
             obj.Text = clock;
         }
-
-        public void UpdatePosition(TextBlock obj,int W, int H)
+        public void makePlaceWords()
         {
-            Thickness temp = obj.Margin;
-            temp.Left = W;
-            temp.Top = H;
-            obj.Margin = temp;
+            words.Add(word1);
+            words.Add(word2);
+            words.Add(word3);
+            words.Add(word4);
+            words.Add(word5);
+            words.Add(word6);
+            words.Add(word7);
+            words.Add(word8);
+            words.Add(word8);
+            words.Add(word9);
+            words.Add(word10);
+            words.Add(word11);
+            words.Add(word12);
+            words.Add(word13);
+            words.Add(word14);
+            words.Add(word15);
+            foreach (RichTextBox word in words)
+            {
+                word.IsEnabled = false;
+                word.IsReadOnly = true;
+                word.FontSize = 40;
+            }
         }
 
-        public void GetTextBlock(TextBlock obj)
+        public void setWord(RichTextBox obj)
         {
-            obj = (TextBlock)this.Resources["word"];
-            obj.Dispatcher.Invoke(new ResizeTextBlockBackCall(this.ResizeTextBlock), new object[] { obj });
-        }
+            TextRange txt = new TextRange(obj.Document.ContentEnd, obj.Document.ContentEnd);
+            txt.Text = lib[new Random().Next(0,lib.Count - 1)];
+            obj.Margin = new Thickness(new Random().Next(0, 500), 0, 0, 0);
 
-        public void ResizeTextBlock(TextBlock obj)
+        }
+        public void setPosition(RichTextBox obj, int top)
         {
-            obj.Measure(new Size(56, 56));
+            obj.Margin = new Thickness( obj.Margin.Left, top, 0, 0);
         }
-
+        public void UpdatePosition()
+        {
+            for (int i = 0; i < 15; i++)
+            {
+                if (words[i].IsEnabled)
+                {
+                    pos[i] += 5;
+                    setPosition(words[i], pos[i]);
+                    if (pos[i] > 768)
+                    {
+                        flag = false;
+                        MessageBox.Show("Game Over!");
+                        break;
+                    }
+                }
+            }
+        }
+        public void CheckandSet()
+        {
+            foreach (RichTextBox word in words)
+            {
+                if (!word.IsEnabled)
+                {
+                    word.IsEnabled = true;
+                    setWord(word);
+                    break;
+                }
+            }
+        }
     }
 }
